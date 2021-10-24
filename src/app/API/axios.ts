f1b03408e79e5dd9a26d0c2662a2cdc524ddeebb
mrden <mr.denislarin@icloud.com>
2021-10-24 15:31:57 +0300

    init

---
+++ src/app/API/axios.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const APIKey = "acd1b13c2d17f4c4ecf126a444d552c5";
+// https://www.behance.net/gallery/129537603/control-panel-weather-dashboard?tracking_source=search_projects_recommended%7Cweather%20website
+
+const axiosInstance = axios.create({
+  baseURL: "",
+});
